1701136a85e5c534542a50a2861028b6c8a35d66
Larissa Cassanjo <larissacassanjo@gmail.com>
2024-06-26 12:00:08 -0300

    Sistema de tentativa e erro

--- batalha-naval.py
+++ batalha-naval.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 import os
 os.system('cls')
 
@@ -19,12 +19,46 @@ print('~'*60)
 # print('|     '*5 + '|')
 # print('|-----'*5 + '|')
 
-navio1 = random.randint(1,25)
-navio2 = random.randint(1,25)
+navio1 = randint(1,25)
+navio2 = randint(1,25)
 if navio2 == navio1:
     navio2 -= 1
-navio3 = random.randint(1,25)
+navio3 = randint(1,25)
 if navio3 == navio1 or navio3 == navio2:
     navio3 += 1
 
-print(navio1, navio2, navio3)
+achou_navio1 = False
+achou_navio2 = False
+achou_navio3 = False
+
+print(navio1,navio2,navio3)
+
+print('Tente afundar os três navios com números de 1 a 25! Você tem 10 tentativas.')
+jogadas = 0
+
+while achou_navio1 == False or achou_navio2 == False or achou_navio3 == False:
+    tentativa = int(input('Onde estão os navios? '))
+    if tentativa == navio1:
+        print('Você acertou o Navio 1!')
+        achou_navio1 = True
+    elif tentativa == navio2:
+        print('Você acertou o Navio 2!')
+        achou_navio2 = True
+    elif tentativa == navio3:
+        print('Você acertou o Navio 3!')
+        achou_navio3 = True
+    else:
+        print('Não tem navios aqui! Tente novamente.')
+    
+    jogadas += 1
+    if jogadas == 10:
+        game_over = True
+        break
+
+if game_over == True:
+    print('Você não encontrou todos os navios.')
+    print('-'*60)
+    print('{:^60}'.format('GAME OVER'))
+    print('-'*60)
+else: 
+    print('Todos os navios foram afundados! Parabéns!')
